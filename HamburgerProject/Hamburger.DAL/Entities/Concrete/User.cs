69f5487c8d67382314249aa5224ff971151e3d00
yucelmerve <yucelmerv@gmail.com>
2024-11-17 16:07:23 +0300

    Connection string, UserType, User Entity

---
+++ HamburgerProject/Hamburger.DAL/Entities/Concrete/User.cs
@@ -0,0 +1,24 @@
+﻿using Hamburger.DAL.Enums;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamburger.DAL.Entities.Concrete
+{
+    public class User : IdentityUser
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public DateOnly BirthDate { get; set; }
+        public UserType UserType { get; set; }
+
+        //public string PictureFileName { get; set; } = string.Empty;
+
+        public DateTime Created { get; set; } = DateTime.Now;
+        public DateTime? Updated { get; set; }
+        public DateTime? Deleted { get; set; }
+    }
+}
---
+++ HamburgerProject/Hamburger.DAL/Enums/UserType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hamburger.DAL.Enums
+{
+    public enum UserType
+    {
+        None = 0,
+        Admin = 1,
+        Employee = 2
+    }
+}
